a847290c652a08ed8e7cc419552c899dd1f08bf7
kau6210 <kazuki_ogawa_aa@icloud.com>
2022-10-28 21:37:37 +0900

    ファイル追加

---
+++ main.js
@@ -0,0 +1,20 @@
+var text = [...
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+]
+
+function convert() {
+    var alphabet = document.getElementById("area1").value;
+    isnoal = alphabet.match(/[^a-z]/gi)
+    if (isnoal) {
+        document.getElementById('result').textContent = 'アルファベットのみ入力してください。';
+    } else {
+        var alphabet = alphabet.toUpperCase();
+        var list = [...alphabet];
+        var resultlist = [];
+        for (var i = 0; i < list.length; i++) {
+            resultlist.push(('00' + text.indexOf(list[i])).slice(-2));
+        }
+        document.getElementById('result').textContent = resultlist.join('');
+
+    }
+}
